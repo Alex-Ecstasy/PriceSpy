42f66062db70e1e683b860d4311783bfced48dea
Alex-Ecstasy <alex29ecstasy@gmail.com>
2023-06-09 18:04:10 +0300

    v 1.9.3 Added update search results from prices with FetchIPA. No need to wait for page refresh Added default rate exchange. It's locate in Prices/Currency.txt A lot of markup

--- PriceSpy.Web/wwwroot/js/Fetch.js
+++ PriceSpy.Web/wwwroot/js/Fetch.js
@@ -1,44 +1,4 @@
-﻿//function sendData() {
-//    var input = document.querySelector("#search-box2").value;
-//    var rate = document.querySelector("#rate-box2").value;
-//    var data = { param1: input, param2: rate };
-//};
-//fetch('/Home/Prices', {
-//    method: 'Post',
-//    headers: {
-//        'Content-Type': 'applicarion/json'
-//    },
-//    body: JSON.stringify(data)
-//})
-//    .then(function (response) {
-//        return response.text();
-//    })
-//    .then(function (result) {
-//        document.querySelector('#accordionPanelsStayOpenExample').innerHTML = result;
-//    });
-    
-//document.querySelector('#search-button2').addEventListener('click', function (e) {
-//    e.preventDefault();
-//    sendData();
-//});
-
-//document.querySelector('#search-box2').addEventListener('keydown', function (e) {
-//    if (e.key === 'Enter') {
-//        e.preventDefault();
-//        sendData();
-//    }
-
-//});
-
-//document.querySelector('#rate-box2').addEventListener('keydown', function (e) {
-//    if (e.key === 'Enter') {
-//        e.preventDefault();
-//        sendData();
-//    }
-
-//});
-
-const form1 = document.querySelector('#search-box2');
+﻿const form1 = document.querySelector('#search-box2');
 const form2 = document.querySelector('#rate-box2');
 const submitButton = document.querySelector('#search-button2');
 
@@ -58,23 +18,23 @@ const submitForm = () => {
         })
         .then(function (result) {
             document.querySelector('#accordionPanelsStayOpenExample').innerHTML = result;
-        });
-        /*.catch(error => console.error(error))*/;
+        })
+        .catch(error => console.error(error));
 }
 
-//form1.addEventListener('keydown', event => {
-//    if (event.key === 'Enter') {
-//        event.preventDefault();
-//        submitForm();
-//    }
-//});
+form1.addEventListener('keydown', event => {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        submitForm();
+    }
+});
 
-//form2.addEventListener('keydown', event => {
-//    if (event.key === 'Enter') {
-//        event.preventDefault();
-//        submitForm();
-//    }
-//});
+form2.addEventListener('keydown', event => {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        submitForm();
+    }
+});
 
 submitButton.addEventListener('click', event => {
     event.preventDefault();

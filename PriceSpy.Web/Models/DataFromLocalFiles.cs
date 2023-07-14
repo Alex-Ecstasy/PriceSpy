893d36094f942a7ffd1d1647c7d4f0f194b3c172
net-alexuse <103592628+net-alexuse@users.noreply.github.com>
2023-07-14 12:30:05 +0300

    Update DataFromLocalFiles.cs
    Fixed fatal error

--- PriceSpy.Web/Models/DataFromLocalFiles.cs
+++ PriceSpy.Web/Models/DataFromLocalFiles.cs
@@ -1,20 +1,25 @@
-﻿namespace PriceSpy.Web.Models
+namespace PriceSpy.Web.Models
 {
     public static class DataFromLocalFiles
     {
-        static string pathWithPrices = (AppDomain.CurrentDomain.BaseDirectory + "Prices");
+        static string pathWithPrices = (AppDomain.CurrentDomain.BaseDirectory);
         public static float GetExchangeRates()
         {
             string File = "/Currency.txt";
             string pathExchangeRates = pathWithPrices + File;
+            FileInfo fileInf = new FileInfo(pathExchangeRates);
+            if (fileInf.Exists)
+            {
+                StreamReader streamReader = new StreamReader(pathExchangeRates);
 
-            StreamReader streamReader = new StreamReader(pathExchangeRates);
-
-            string? rate = streamReader.ReadLine();
-            if (!string.IsNullOrEmpty(rate)) rate = rate.Replace(".", ",");
-            if (!float.TryParse(rate, out float rateExchange)) rateExchange = 1;
-            Console.WriteLine(rateExchange);
-            return rateExchange;
+                string? rate = streamReader.ReadLine();
+                if (!string.IsNullOrEmpty(rate)) rate = rate.Replace(".", ",");
+                if (!float.TryParse(rate, out float rateExchange)) rateExchange = 1;
+                Console.WriteLine(rateExchange);
+                return rateExchange;
+            }
+            else return 1;
+            
         }
     }
 }

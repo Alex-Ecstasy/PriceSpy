79292974b04a475f19b3a25570b68c263bb0ec46
Alex-Ecstasy <alex29ecstasy@gmail.com>
2024-10-23 16:47:36 +0300

    Added price setting algorith to the DB

--- PriceSpy.Web/Models/DataBaseHandler.cs
+++ PriceSpy.Web/Models/DataBaseHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Data.Sqlite;
+using System.Runtime.CompilerServices;
 
 namespace PriceSpy.Web.Models
 {
@@ -9,13 +10,12 @@ namespace PriceSpy.Web.Models
         private static readonly string _dbConnection = "DataSource=" + _dbFile;
         public static void FindElementInDb(Card card, SqliteConnection connection)
         {
-            string id = card.CardUrl;
             var productID = card.UrlPrefix + card.CardUrl;
             string selectQuery = "SELECT * FROM Products WHERE ID = @id";
             using SqliteCommand command = new(selectQuery, connection);
             command.Parameters.AddWithValue("@id", productID);
-            using var reader = command.ExecuteReader();
-            if (reader.HasRows) SetPrices(card, connection); 
+            var reader = command.ExecuteReader();
+            if (reader.HasRows) SetPrices(card, reader, connection); 
             else InsertElement(card, connection);
         }
         public static async Task<SqliteConnection> LoadAsync()
@@ -41,10 +41,10 @@ namespace PriceSpy.Web.Models
                 string createTableQuery = $@"
                     CREATE TABLE IF NOT EXISTS Products (
                         ID TEXT PRIMARY KEY,
-                        CurrentPrice REAL,
+                        ActualPrice REAL,
                         MinPrice REAL,
                         MaxPrice REAL,
-                        CurrentDate TEXT,
+                        ActualDate TEXT,
                         MinPriceDate TEXT,
                         MaxPriceDate TEXT
                     );";
@@ -65,28 +65,68 @@ namespace PriceSpy.Web.Models
         {
             if (card.IsAvailable && card.Price != 0)
             {
-                string insertQuery = @"INSERT INTO Products (ID, CurrentPrice, CurrentDate)
-                               VALUES (@id, @currentPrice, @currentDate)";
-                var currentDate = DateTime.Now;
+                string insertQuery = @"INSERT INTO Products (ID, ActualPrice, ActualDate)
+                               VALUES (@id, round(@actualPrice, 2), @actualDate)";
+                var actualDate = DateTime.Now;
                 var productID = card.UrlPrefix + card.CardUrl;
-                //var price = card.Price.ToString();
                 using var insertCommand = new SqliteCommand(insertQuery, connection);
                 insertCommand.Parameters.AddWithValue("@id", productID);
-                insertCommand.Parameters.AddWithValue("@currentPrice", card.Price);
-                insertCommand.Parameters.AddWithValue("@currentDate", currentDate);
+                insertCommand.Parameters.AddWithValue("@actualPrice", card.Price);
+                insertCommand.Parameters.AddWithValue("@actualDate", actualDate);
                 insertCommand.ExecuteNonQuery();
             }
         }
-        private static void SetPrices(Card card, SqliteConnection connection)
+        private static void SetPrices(Card card, SqliteDataReader reader, SqliteConnection connection)
         {
-            //if (existingPrice > currentPrice)
+            if (reader.Read())
             {
-                string updateMinPriceQuery = "UPDATE Products SET MinPrice = @minPrice, MinPriceDate = @minPriceDate WHERE ID = @id";
+                float? currentPrice = card.Price;
+                float? actualPriceDb = reader.GetFloat(1);
+                float? minPriceDb = reader.GetFloat(2);
+                float? maxPriceDb = reader.GetFloat(3);
+                if (currentPrice == actualPriceDb)
+                {
+                    //Console.WriteLine($@"Parse {actualPriceDb} " + card.Name);
+                    return;
+                }
+
+                //if (currentPrice > actualPriceDb)
+                //{
+                //    string updateMinPriceQuery = "UPDATE Products SET MinPrice = @minPrice, MinPriceDate = @minPriceDate WHERE ID = @id";
+                //    using var updateCommand = new SqliteCommand(updateMinPriceQuery, connection);
+                //    var productID = card.UrlPrefix + card.CardUrl;
+                //    updateCommand.Parameters.AddWithValue("@id", productID);
+                //    updateCommand.Parameters.AddWithValue("@actualPrice", card.Price);
+                //    updateCommand.Parameters.AddWithValue("@actualDate", actualDate);
+                //}
+                //else
+                //{
+                //    string updateMaxPriceQuery = "UPDATE Products SET MaxPrice = @maxPrice, MaxPriceDate = @maxPriceDate WHERE ID = @id";
+                //}
+
+                if (currentPrice > actualPriceDb)
+                {//     10              1
+                    currentPrice = actualPriceDb; //currentPrice < maxPriceDb
+                    //       10             9
+                    if (currentPrice >= maxPriceDb)
+                    {
+                        maxPriceDb = currentPrice;
+                    }
+
+                }
+
+                if (currentPrice < actualPriceDb)
+                {//          1             10
+                    currentPrice = actualPriceDb; //currentPrice > minPriceDb
+                    //       1              10
+                    if (currentPrice <= minPriceDb)
+                    {
+                        minPriceDb = currentPrice;
+                    }
+                }
+
             }
-            //else if (existingPrice < currentPrice)
-            {
-                string updateMaxPriceQuery = "UPDATE Products SET MaxPrice = @maxPrice, MaxPriceDate = @maxPriceDate WHERE ID = @id";
-            }
+
         }
     }
     
